fc2646e3077f1fe1cb2e78b21fdcaf70385d46b9
honchokomodo <70309110+honchokomodo@users.noreply.github.com>
2021-07-11 20:12:07 -0700

    did some refactoring(ig) hope nothing breaks

--- clock.js
+++ clock.js
@@ -1,17 +1,8 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
+var ctx = document.getElementById("myCanvas").getContext("2d");
 var lose = false;
-var width = myCanvas.width;
-var height = myCanvas.height;
-var centerX = width/2;
-var centerY = height/2;
-var radius = Math.min(width, height) / 3;
 var rot = 0
 var two_pi = 2 * Math.PI;
 var goal = Math.random() * two_pi;
-var press = false;
-var alrpress = false;
 var score = 0;
 var tops = 0;
 
@@ -32,46 +23,50 @@ function drawLine(x1, y1, x2, y2, width, color) {
   ctx.stroke();
 }
 
-function ui_mouseup(MyEvent) {if (MyEvent.which == 1) {press = false;}}
-function ui_mousedown(MyEvent) {if (MyEvent.which == 1) {press = true;}}
-
-function drawFrame() {
-  if (lose) {
-    if (press && !alrpress) {
+function ui_mousedown(MyEvent) {
+  if (MyEvent.which == 1) {
+    if (lose) {
       score = 0;
       lose = false;
+      return;
     }
-    alrpress = press;
-    return;
-  }
-  
-  rot = (rot + 0.01 * Math.pow(1.05, score) * (score%2*2-1)) % two_pi;
-  vecX = Math.cos(rot);
-  vecY = Math.sin(rot);
-  
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  drawArc(centerX, centerY, 0, two_pi, radius, 2, "#FFF");
-  drawArc(centerX, centerY, goal-0.1, goal+0.1, radius*1.15, 2, "#0F0");
-  drawLine(centerX+radius*1.05*vecX, centerY+radius*1.05*vecY, centerX+radius*1.25*vecX, centerY+radius*1.25*vecY, 2, "#FFF");
-  ctx.fillStyle = "#FFF";
-  ctx.fillText("score: "+score, 10, 20);
-  ctx.fillText("session high score: "+tops, 10, 30);
-  
-  if (press && !alrpress) {
-    goalX = Math.cos(goal);
-    goalY = Math.sin(goal);
-    if (Math.pow(vecX-goalX, 2)+Math.pow(vecY-goalY, 2) > 0.0169) {
+    
+    var handX = Math.cos(rot);
+    var handY = Math.sin(rot);
+    var goalX = Math.cos(goal);
+    var goalY = Math.sin(goal);
+    if (Math.pow(handX-goalX, 2)+Math.pow(handY-goalY, 2) > 0.0169) {
       lose = true;
-      alrpress = true;
       return;
     }
     score++;
     goal = Math.random() * two_pi;
     tops = Math.max(tops, score);
   }
-  alrpress = press;
+}
+
+function drawFrame() {
+  if (lose) {
+    return;
+  }
+  
+  var width = myCanvas.width;
+  var height = myCanvas.height;
+  var centerX = width/2;
+  var centerY = height/2;
+  var radius = Math.min(width, height) / 3;
+  var vecX = Math.cos(rot);
+  var vecY = Math.sin(rot);
+  rot = (rot + 0.01 * Math.pow(1.05, score) * (score%2*2-1)) % two_pi;
+  
+  ctx.clearRect(0, 0, width, height);
+  drawArc(centerX, centerY, 0, two_pi, radius, 2, "#FFF");
+  drawArc(centerX, centerY, goal-0.1, goal+0.1, radius*1.15, 2, "#0F0");
+  drawLine(centerX+radius*1.05*vecX, centerY+radius*1.05*vecY, centerX+radius*1.25*vecX, centerY+radius*1.25*vecY, 2, "#FFF");
+  ctx.fillStyle = "#FFF";
+  ctx.fillText("score: "+score, 10, 20);
+  ctx.fillText("session high score: "+tops, 10, 30);
 }
 
 setInterval(drawFrame, 1000/60);
-myCanvas.addEventListener("pointerup", ui_mouseup);
 myCanvas.addEventListener("pointerdown", ui_mousedown);
