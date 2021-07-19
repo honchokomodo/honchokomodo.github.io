46f6572b3c59ceeb022b59550f68f59cf03893ab
honchokomodo <70309110+honchokomodo@users.noreply.github.com>
2021-07-18 22:48:33 -0700

    hopefully fixed an annoying bug

--- clock.js
+++ clock.js
@@ -5,6 +5,7 @@ var two_pi = 2 * Math.PI;
 var goal = Math.random() * two_pi;
 var score = 0;
 var tops = 0;
+var pflag = false;
 
 function drawArc(x, y, r1, r2, radius, width, color) {
   ctx.strokeStyle = color;
@@ -25,12 +26,40 @@ function drawLine(x1, y1, x2, y2, width, color) {
 
 clockCanvas.addEventListener("pointerdown", function(MyEvent) {
   if (MyEvent.which == 1) {
+    pflag = true;
+  }
+});
+
+setInterval(function() {
+  rot = (rot+0.01*Math.pow(1.05, score) * (score%2*2-1) * !lose) % two_pi;
+  clockCanvas.width = window.innerWidth - 16;
+  clockCanvas.height = window.innerHeight - 40;
+  var width = clockCanvas.width;
+  var height = clockCanvas.height;
+  var centerX = width/2;
+  var centerY = height/2;
+  var radius = Math.min(width, height) / 3;
+  var vecX = Math.cos(rot);
+  var vecY = Math.sin(rot);
+  
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, width, height);
+  drawArc(centerX, centerY, 0, two_pi, radius, 4, "#FFF");
+  drawArc(centerX, centerY, goal-0.1, goal+0.1, radius*1.15, 4, "#0F0");
+  drawLine(centerX+radius*1.05*vecX, centerY+radius*1.05*vecY, centerX+radius*1.25*vecX, centerY+radius*1.25*vecY, 4, "#FFF");
+  ctx.fillStyle = "#FFF";
+  ctx.font = "32px consolas"
+  ctx.fillText("score: "+score, 10, 32);
+  ctx.fillText("high score: "+tops, 10, 64);
+
+  if (pflag) {
+    pflag = false;
     if (lose) {
       score = 0;
       lose = false;
       return;
     }
-    
     var handX = Math.cos(rot);
     var handY = Math.sin(rot);
     var goalX = Math.cos(goal);
@@ -44,30 +73,4 @@ clockCanvas.addEventListener("pointerdown", function(MyEvent) {
     goal = Math.random() * two_pi;
     tops = Math.max(tops, score);
   }
-});
-
-setInterval(function() {
-  if (lose) {return;}
-  
-  clockCanvas.width = window.innerWidth - 16;
-  clockCanvas.height = window.innerHeight - 40;
-  var width = clockCanvas.width;
-  var height = clockCanvas.height;
-  var centerX = width/2;
-  var centerY = height/2;
-  var radius = Math.min(width, height) / 3;
-  var vecX = Math.cos(rot);
-  var vecY = Math.sin(rot);
-  rot = (rot+0.01*Math.pow(1.05, score) * (score%2*2-1)) % two_pi;
-  
-  ctx.clearRect(0, 0, width, height);
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, width, height);
-  drawArc(centerX, centerY, 0, two_pi, radius, 4, "#FFF");
-  drawArc(centerX, centerY, goal-0.1, goal+0.1, radius*1.15, 4, "#0F0");
-  drawLine(centerX+radius*1.05*vecX, centerY+radius*1.05*vecY, centerX+radius*1.25*vecX, centerY+radius*1.25*vecY, 4, "#FFF");
-  ctx.fillStyle = "#FFF";
-  ctx.font = "32px consolas"
-  ctx.fillText("score: "+score, 10, 32);
-  ctx.fillText("high score: "+tops, 10, 64);
 }, 1000/60);
