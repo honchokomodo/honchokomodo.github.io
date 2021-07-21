5de0b8e82a53de481da16c505fc4def2a48f3397
honchokomodo <70309110+honchokomodo@users.noreply.github.com>
2021-07-20 20:34:31 -0700

    shortened it a bit more

--- clock.js
+++ clock.js
@@ -1,8 +1,8 @@
 var ctx = document.getElementById("clockCanvas").getContext("2d");
 let lose = false;
 let rot = 0
-let two_pi = 2 * Math.PI;
-let goal = Math.random() * two_pi;
+let tau = 2 * Math.PI;
+let goal = Math.random()*tau;
 let score = 0;
 let tops = 0;
 let pflag = false;
@@ -25,21 +25,21 @@ function drawLine(x1, y1, x2, y2, width, color) {
 }
 
 setInterval(() => {
-  clockCanvas.width = window.innerWidth - 16;
-  clockCanvas.height = window.innerHeight - 40;
+  clockCanvas.width = window.innerWidth-16;
+  clockCanvas.height = window.innerHeight-40;
   let width = clockCanvas.width;
   let height = clockCanvas.height;
-  let centerX = width/2;
-  let centerY = height/2;
-  let radius = Math.min(width, height) / 3;
+  let cenX = width/2;
+  let cenY = height/2;
+  let radius = Math.min(width, height)/3;
   let vecX = Math.cos(rot);
   let vecY = Math.sin(rot);
   ctx.clearRect(0, 0, width, height);
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, width, height);
-  drawArc(centerX, centerY, 0, two_pi, radius, 4, "#FFF");
-  drawArc(centerX, centerY, goal-0.1, goal+0.1, radius*1.15, 4, "#0F0");
-  drawLine(centerX+radius*1.05*vecX, centerY+radius*1.05*vecY, centerX+radius*1.25*vecX, centerY+radius*1.25*vecY, 4, "#FFF");
+  drawArc(cenX, cenY, 0, tau, radius, 4, "#FFF");
+  drawArc(cenX, cenY, goal-0.1, goal+0.1, radius*1.15, 4, "#0F0");
+  drawLine(cenX+radius*1.05*vecX, cenY+radius*1.05*vecY, cenX+radius*1.25*vecX, cenY+radius*1.25*vecY, 4, "#FFF");
   ctx.fillStyle = "#FFF";
   ctx.font = "32px consolas"
   ctx.fillText("score: "+score, 10, 32);
@@ -62,11 +62,11 @@ setInterval(() => {
       return;
     }
     score++;
-    goal = Math.random() * two_pi;
+    goal = Math.random() * tau;
     tops = Math.max(tops, score);
   }
 
-  rot = (rot+0.01*Math.pow(1.05, score) * (score%2*2-1) * !lose) % two_pi;
+  rot = (rot+0.01*Math.pow(1.05, score) * (score%2*2-1) * !lose) % tau;
 }, 1000/60);
 
 clockCanvas.addEventListener("pointerdown", () => {pflag = true});
